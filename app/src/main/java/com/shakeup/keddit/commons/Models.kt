44aee99995bad88ec3a634d1a159a2f35c78e790
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-09-05 15:46:12 -0700

    Implemented rotation state saving.

--- app/src/main/java/com/shakeup/keddit/commons/Models.kt
+++ app/src/main/java/com/shakeup/keddit/commons/Models.kt
@@ -1,11 +1,40 @@
 package com.shakeup.keddit.commons
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.shakeup.keddit.commons.adapter.AdapterConstants
 import com.shakeup.keddit.commons.adapter.ViewType
 
 /**
  * Created by Jayson on 8/30/2017.
  */
+data class RedditNews(
+        val after: String,
+        val before: String,
+        val news: List<RedditNewsItem>) : Parcelable {
+
+    companion object {
+        @JvmField @Suppress("unused")
+        val CREATOR = createParcel { RedditNews(it) }
+    }
+
+    protected constructor(parcelIn: Parcel) : this(
+            parcelIn.readString(),
+            parcelIn.readString(),
+            mutableListOf<RedditNewsItem>().apply {
+                parcelIn.readTypedList(this, RedditNewsItem.CREATOR)
+            }
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(after)
+        dest.writeString(before)
+        dest.writeTypedList(news)
+    }
+
+    override fun describeContents() = 0
+}
+
 data class RedditNewsItem(
         val author: String,
         val title: String,
@@ -13,11 +42,32 @@ data class RedditNewsItem(
         val created: Long,
         val thumbnail: String,
         val url: String
-) : ViewType {
+) : ViewType, Parcelable {
+
+    companion object {
+        @JvmField @Suppress("unused")
+        val CREATOR = createParcel { RedditNewsItem(it) }
+    }
+
+    protected constructor(parcelIn: Parcel) : this(
+            parcelIn.readString(),
+            parcelIn.readString(),
+            parcelIn.readInt(),
+            parcelIn.readLong(),
+            parcelIn.readString(),
+            parcelIn.readString()
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(author)
+        dest.writeString(title)
+        dest.writeInt(numComments)
+        dest.writeLong(created)
+        dest.writeString(thumbnail)
+        dest.writeString(url)
+    }
+
+    override fun describeContents() = 0
+
     override fun getViewType() = AdapterConstants.NEWS
 }
-
-data class RedditNews(
-        val after: String,
-        val before: String,
-        val news: List<RedditNewsItem>)
